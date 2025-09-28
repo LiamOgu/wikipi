53c77aba298e1a79ccd123126003b8730d79db8b
Lucas-132 <lucas.pollet135@gmail.com>
2025-09-28 19:38:46 +0200

    comment: added one comment containing instructions to follow before unsing the component inside the final page's file

--- src/components/MainWelcomeCard.jsx
+++ src/components/MainWelcomeCard.jsx
@@ -1,7 +1,9 @@
 const MainWelcomeCard = () => {
     return (
-        <div className="w-full flex justify-center h-[230px]">
-            <div className="hero bg-[#C81E1E] rounded-md w-9/10">
+        <div className="w-full flex justify-center"> {/*This div is here to simulate the div in which the card will be in, 
+        please remove it before using it inside the final website code*/}
+
+            <div className="hero bg-[#C81E1E] rounded-md w-9/10 h-[230px]">
                 <div className="hero-content flex-col justify-between lg:flex-row w-10/10">
                     <div>
                         <p>La base documentaire pour le Labo</p>
