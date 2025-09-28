bb50184fd5de6da3e80d7ad49c5d2d2744f068f0
LïamA <augu.liam@gmail.com>
2025-09-28 18:08:35 +0200

    header images importations

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,19 @@
 import logoWikipi from '../assets/Logo_wikiPi.png'
+import defaultUser from '../assets/default-user-icon.wepb'
 
 const Navbar = () => {
   return (
     <div>
-      <img src={logoWikipi} class="h-12 me-3" alt="Wikipi Logo"></img>
+      <div>
+        <img src={logoWikipi} class="h-12 me-3" alt="Wikipi Logo"></img>
+      </div>
+      <div>
+        <div className="avatar">
+          <div className="w-24 rounded-full">
+            <img src={defaultUser} alt="" />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
