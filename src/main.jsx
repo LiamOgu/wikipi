5c65e4e7a121250c9fbf23b8bdf575d55837689f
LïamA <augu.liam@gmail.com>
2025-09-28 20:02:50 +0200

    Mise en place du setup de routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import Home from './routes/Home';
+import Login from './routes/Login';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/Login',
+    element: <Login />,
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
