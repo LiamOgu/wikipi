e992c6c9aad955d229f3ca18bc70c7a0632fc242
LïamA <augu.liam@gmail.com>
2025-09-28 19:08:53 +0200

    Navbar with Profile
    Enhanced the Navbar component with a user avatar and dropdown menu, improved layout and responsiveness, and removed the unused Profile component from the project and App layout.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import Navbar from "./components/Navbar"
-import Profile from "./components/Profile"
 
 function App() {
   return (
     <div>
       <header className="flex justify-between">
         <Navbar/>
-        <Profile/>
       </header>
       
     </div>
