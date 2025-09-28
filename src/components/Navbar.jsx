76b37791f81ae41f157cb074b0e939618a942bca
LïamA <augu.liam@gmail.com>
2025-09-28 17:57:48 +0200

    importation de daisiui

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,10 @@
+import logoWikipi from '../assets/Logo_wikiPi.png'
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+      <img src={logoWikipi} class="h-12 me-3" alt="Wikipi Logo"></img>
+    </div>
   )
 }
 
