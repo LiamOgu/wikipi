b0d01ff0e8eeb2cbd2e1c1af26a67f996952f6e6
LïamA <augu.liam@gmail.com>
2025-10-05 16:40:34 +0200

    Update Home.jsx

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,10 +1,14 @@
 import Navbar from "../components/Navbar"
+import Sidebar from "./components/Sidebar"
 
 const Home = () => {
   return (
     <div>
-      <Navbar />
-      <div>Home</div>
+      <header className="flex justify-between">
+        <Navbar/>
+      </header>
+      <Sidebar/>
+       
     </div>
   )
 }
