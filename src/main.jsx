d3313111848421e58a9258c6803b897169889be6
LïamA <augu.liam@gmail.com>
2025-10-28 08:50:56 +0100

    register / login routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './routes/Home';
 import Login from './routes/Login';
+import Register from './routes/Register';
 
 const router = createBrowserRouter([
   {
@@ -13,7 +14,11 @@ const router = createBrowserRouter([
   {
     path: '/Login',
     element: <Login />,
-  }
+  },
+  {
+    path: '/Register',
+    element: <Register />,
+  },
 ]);
 
 createRoot(document.getElementById('root')).render(
