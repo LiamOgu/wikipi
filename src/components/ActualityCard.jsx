0bf4fbe94674f4f625df904a7510991083f109bd
LïamA <augu.liam@gmail.com>
2025-11-18 11:13:45 +0100

    projet wikipi -> vrai nom du projet associé

--- src/components/ActualityCard.jsx
+++ src/components/ActualityCard.jsx
@@ -1,12 +1,17 @@
 import user from '../assets/default-user-icon.webp'
-import data from '../data/documentationsData.js'
+import docData from '../data/documentationsData.js'
+import projetData from '../data/projetsData.js'
 
 const ActualityCard = ({ id }) => {
-    const documentation = data.documentation.find(doc => doc.id === id)
+    const documentation = docData.documentation.find(doc => doc.id === id)
+    if (!documentation) {
+        return <div>Documentation introuvable (id: {id})</div>
+    }
+    const projet = projetData.projet.find(proj => proj.documentations.some(doc => doc.id === id));
     return (
         <div className="card w-full bg-white card-sm shadow-sm hover:shadow-lg hover:scale-105 transition-all duration-200 hover:cursor-pointer">
             <div className="card-body text-black">
-                <h3>projet wikipi</h3>
+                <h3>{projet ? projet.title : "Projet inconnu"}</h3>
                 <h2 className="card-title">{documentation.title}</h2>
                 <p>{documentation.content}</p>
                 <div className='flex flex-row gap-4'>
