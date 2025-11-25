fdad0ecdf1110d660b0dec886373e91abeefd03d
Lucas-132 <lucas.pollet135@gmail.com>
2025-11-25 11:59:09 +0100

    fix: cursor-pointer when mouse hovers the link

--- src/routes/Project.jsx
+++ src/routes/Project.jsx
@@ -24,7 +24,7 @@ const Project = () => {
             <h2 className="text-3xl font-bold">{documentation ? documentation.title : "Documentation inconnue"}</h2>
           </div>
 
-          <a className="text-blue-500">soumettre une modification</a>
+          <a className="text-blue-500 hover:cursor-pointer">soumettre une modification</a>
         </div>
         
         <p>{documentation ? documentation.content : "Contenu indisponible"}</p>
