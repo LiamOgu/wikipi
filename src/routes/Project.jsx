72616ef241a3dada235805e014efbdd9eb2e1743
LïamA <augu.liam@gmail.com>
2025-11-25 08:51:09 +0100

    Integrate Navbar and Sidebar into Project route

--- src/routes/Project.jsx
+++ src/routes/Project.jsx
@@ -2,19 +2,28 @@ import DocumentPage from "../components/DocumentPage"
 import { useSearchParams } from "react-router-dom"
 import docData from '../data/documentationsData.js'
 import projetData from '../data/projetsData.js'
+import Navbar from "../components/Navbar"
+import Sidebar from "../components/Sidebar"
 
 const Project = () => {
   const [searchParams] = useSearchParams()
   const title = searchParams.get("title")
   const doc = searchParams.get("doc")
-  const product = projetData.projet.find(product => product.title === title)
+  const project = projetData.projet.find(project => project.title === title)
   const documentation = docData.documentation.find(documentation => documentation.title === doc)
   return (
-    <div>
-      <h1>{product ? product.title : "Projet inconnu"}</h1>
-      <h2>{documentation ? documentation.title : "Documentation inconnue"}</h2>
-      <p>{documentation ? documentation.content : "Contenu indisponible"}</p>
-    </div>
+    <Sidebar>
+      <header >
+        <Navbar />
+      </header>
+      <main className="p-6 mt-30">
+        <DocumentPage project={project} documentation={documentation} />
+        <h1>{project ? project.title : "Projet inconnu"}</h1>
+        <h2>{documentation ? documentation.title : "Documentation inconnue"}</h2>
+        <p>{documentation ? documentation.content : "Contenu indisponible"}</p>
+      </main>
+    </Sidebar >
+
   )
 }
 
