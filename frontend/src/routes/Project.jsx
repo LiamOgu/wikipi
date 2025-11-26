91dfe1d469de5d2273392bc87cd7c3339816fca9
LïamA <augu.liam@gmail.com>
2025-11-26 11:02:06 +0100

    fix, window scroll to the top

--- frontend/src/routes/Project.jsx
+++ frontend/src/routes/Project.jsx
@@ -4,8 +4,12 @@ import docData from '../data/documentationsData.js'
 import projetData from '../data/projetsData.js'
 import Navbar from "../components/Navbar"
 import Sidebar from "../components/Sidebar"
+import { useEffect } from "react"
 
 const Project = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
   const [searchParams] = useSearchParams()
   const title = searchParams.get("title")
   const doc = searchParams.get("doc")
@@ -17,7 +21,7 @@ const Project = () => {
         <Navbar />
       </header>
       <main className="p-6 mt-30 mx-5 border-1 border-dashed border-gray-200 rounded">
-        
+
         <div className="flex justify-between">
           <div className="flex flex-col mb-4 gap-5 lg:flex-row lg:gap-10">
             <h1 className="text-3xl font-bold">{project ? project.title : "Projet inconnu"}</h1>
@@ -26,7 +30,7 @@ const Project = () => {
 
           <a className="text-blue-500 hover:cursor-pointer">soumettre une modification</a>
         </div>
-        
+
         <p>{documentation ? documentation.content : "Contenu indisponible"}</p>
       </main>
     </Sidebar >
