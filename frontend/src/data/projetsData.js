d23c35c2fe98f82d22a9c35351e06d77a741c973
LïamA <augu.liam@gmail.com>
2025-12-03 11:57:46 +0100

    Update projetsData.js

--- frontend/src/data/projetsData.js
+++ frontend/src/data/projetsData.js
@@ -1,39 +1,54 @@
 const data = {
   projet: [
     {
-      id : 0,
-      title : "Projet Labo 1",
-      documentations : [
+      id: 0,
+      title: "Projet Labo 1",
+      documentations: [
         {
-          id : 0
-        }
-      ]
+          id: 0,
+        },
+      ],
     },
     {
-      id : 1,
-      title : "Projet Labo 2",
-      documentations : [
+      id: 1,
+      title: "Projet Labo 2",
+      documentations: [
         {
-          id : 1
-        }
-      ]
+          id: 1,
+        },
+      ],
     },
     {
-      id : 2,
-      title : "Projet Labo 3",
-      documentations : [
+      id: 2,
+      title: "Projet Labo 3",
+      documentations: [
         {
-          id : 2
+          id: 2,
         },
         {
-          id : 3
+          id: 3,
         },
         {
-          id : 4
-        }
-      ]
-    }
-  ]
-}
+          id: 4,
+        },
+      ],
+    },
+    {
+      id: 3,
+      title: "Wikipi",
+      documentations: [
+        {
+          id: 2,
+        },
+        {
+          id: 3,
+        },
+        {
+          id: 4,
+        },
+      ],
+    },
+  ],
+};
 
 export default data;
