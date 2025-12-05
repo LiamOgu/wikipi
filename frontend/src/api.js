189c945c1ef37e3ce8a8517f6ddc3c64225982be
LïamA <augu.liam@gmail.com>
2025-12-05 20:39:19 +0100

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // Configuration unique d'axios
 export const api = axios.create({
   baseURL: "http://localhost:3000",
-  timeout: 5000,
+  timeout: 10000,
   headers: {
     "Content-Type": "application/json",
   },
