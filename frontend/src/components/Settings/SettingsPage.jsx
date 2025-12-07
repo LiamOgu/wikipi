1f095176017e9a6ef268b66a1d3e25ad33760aea
Lucas-132 <lucas.pollet135@gmail.com>
2025-12-07 16:29:42 +0100

    added submit button and hover effect to user avatar

--- frontend/src/components/Settings/SettingsPage.jsx
+++ frontend/src/components/Settings/SettingsPage.jsx
@@ -6,7 +6,7 @@ const SettingsPage = () => {
 
   return (
     <div className="flex flex-col gap-5">
-      <h1 className="text-4xl font-bold">Paramètres</h1>
+      <h1 className="text-4xl font-bold">Paramètres du compte</h1>
       <form className="flex flex-col gap-5">
         <h2>Changer de nom :</h2>
         <input
@@ -22,14 +22,22 @@ const SettingsPage = () => {
           className="border p-2 rounded-xl border-dashed border-gray-300"
         />
         <h2>Changer l'image de profil:</h2>
-        <img src={user} alt="User Avatar" className="w-50 rounded-full" />
-        <h2>Thème:</h2>
+        <img src={user} alt="User Avatar" className="w-50 rounded-full hover:cursor-pointer" />
         <input
-          type="checkbox"
-          className="toggle border-indigo-600 bg-indigo-500 checked:border-orange-500 checked:bg-orange-400 checked:text-orange-800"
+          type="submit"
+          value={"Sauvegarder les modifications"}
+          className="my-10 py-3 hover:cursor-pointer bg-red-secondary rounded-md text-white font-medium"
         />
       </form>
-      
+
+      <hr />
+
+      <h1 className="text-4xl font-bold">Préférences</h1>
+      <h2>Thème:</h2>
+      <input
+        type="checkbox"
+        className="toggle border-indigo-600 bg-indigo-500 checked:border-orange-500 checked:bg-orange-400 checked:text-orange-800"
+      />
     </div>
   );
 };
